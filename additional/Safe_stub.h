3d5c5b0e1d53649b86a80843be3b73ee77c478e5
rememmber <oldyear2005@mail.ru>
2015-07-27 14:39:40 +0100

    Create Safe_stub.h

---
+++ additional/Safe_stub.h
@@ -0,0 +1,10 @@
+#include "HsFFI.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+extern void process_network_input(HsPtr a1, HsPtr a2, HsPtr a3, HsPtr a4, HsPtr a5, HsPtr a6, HsPtr a7, HsPtr a8);
+extern void train(HsPtr a1);
+#ifdef __cplusplus
+}
+#endif
+
